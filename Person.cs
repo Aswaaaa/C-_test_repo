b50a2269a24696bf8ce34e7c5e1f5388cfd8d85d
Aswaaaa <aswanth.k@edstem.com>
2024-10-21 13:59:01 +0530

    first C# code

---
+++ Person.cs
@@ -0,0 +1,13 @@
+namespace MyCSharpAPP
+{
+    public class Person{
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce(){
+            System.Console.WriteLine("My name is " + FirstName + " " + LastName);
+    }
+
+
+}
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyCSharpAPP.Math;
+
+namespace MyCSharpAPP
+{
+    class Program{
+    static void Main(string[] args){
+        var person = new Person();
+        person.FirstName = "Aswa";
+        person.LastName = " ";
+        person.Introduce();
+
+        Calculator calculator = new Calculator();
+        int result = calculator.Add(1,5);
+        System.Console.WriteLine(result);
+    }
+}
+}
